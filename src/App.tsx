14218e8e1205e2da00b9bbe80214d8a52214c3d4
Rahul Sethuram <rahul@sethuram.com>
2020-10-27 17:04:15 +0100

    🚀 Works

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserNode } from "@connext/vector-browser-node";
+import { ChannelSigner } from "@connext/vector-utils";
+import pino from "pino";
+import { Wallet } from "ethers";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const effect = async () => {
+      const wallet = Wallet.createRandom();
+      const signer = new ChannelSigner(wallet.privateKey);
+      const node = await BrowserNode.connect({
+        chainAddresses: {
+          "4": {
+            channelMastercopyAddress:
+              "0x048D978cEa968a6D27Ef1C792Cb1bA83A9A5bBc9",
+            channelFactoryAddress: "0x70b3673264E9D56Cc8e53597bF3105eF281a5a0c",
+            transferRegistryAddress:
+              "0x268ee7145a1c717d36dAfcF402b51ac4BAdD6388",
+          },
+        },
+        chainProviders: "https://rinkeby.infura.io/v3/",
+        logger: pino(),
+        signer,
+        messagingUrl: "https://messaging.connext.network",
+      });
+      console.log(`Node connected! ${node.publicIdentifier}`);
+    };
+    effect();
+  });
   return (
     <div className="App">
       <header className="App-header">
